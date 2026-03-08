09a7ab21b6922a44fc76e4c6ee60be770fb2f5f3
johanna1290aguirre-web <johanna1290aguirre-web@users.noreply.github.com>
2026-03-07 20:46:46 -0500

    feat: configurar CORS para producción en Vercel

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ console.log('DB_NAME:', process.env.DB_NAME);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'https://ecommerce-saludable-react.vercel.app',
+  credentials: true
+}));
 
 // Rutas de la API
 app.use('/api/productos', productRoutes);
